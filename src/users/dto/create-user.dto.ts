e05a7abe953ff99829c22792d503df696f4afe37
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-11-28 16:26:53 -0300

    conventional commit

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,7 @@
 
 
 export class CreateUserDto {
+   
     name: string;
     nickname: string;
     email: string;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,16 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService){}
+  users: User[] = [];
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
+    const user: User = {
+      id: 'random_id',
+
+      ...createUserDto,
+    };
+
+    this.users.push(user);
     return 'This action adds a new user';
   }
 
   findAll() {
-    return this.prisma;
+    return this.users;
   }
 
   findOne(id: number) {
