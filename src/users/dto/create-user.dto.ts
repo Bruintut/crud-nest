212b774eab1691684c4a52bc3bb1782ac5e7dc64
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-11-28 17:25:09 -0300

    Swagger implement

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,27 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail } from "class-validator";
 
 
 export class CreateUserDto {
-   
+    @ApiProperty({
+        description:'Nome do Usuário'
+    })
     name: string;
+
+    @ApiProperty({
+        description:'Nicname do Usuário'
+    })
     nickname: string;
+
+    @IsEmail()
+    @ApiProperty({
+        description:'Email do Usuário'
+    })
     email: string;
+
+    @ApiProperty({
+        description:'Senha do Usuário'
+    })
     password: string;
     
 }
