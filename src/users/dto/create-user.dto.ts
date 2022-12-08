d8d13ab99f6e7eb6964a19720eecfd8728fd328c
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-12-08 18:32:56 -0300

    feat: Add new property to User

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,27 +1,30 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail } from "class-validator";
-
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail } from 'class-validator';
 
 export class CreateUserDto {
-    @ApiProperty({
-        description:'Nome do Usuário'
-    })
-    name: string;
+  @ApiProperty({
+    description: 'Nome do Usuário',
+  })
+  name: string;
 
-    @ApiProperty({
-        description:'Nicname do Usuário'
-    })
-    nickname: string;
+  @ApiProperty({
+    description: 'Nicname do Usuário',
+  })
+  nickname: string;
 
-    @IsEmail()
-    @ApiProperty({
-        description:'Email do Usuário'
-    })
-    email: string;
+  @ApiProperty({
+    description: 'CPF do Usuário',})
+    cpf: string;
 
-    @ApiProperty({
-        description:'Senha do Usuário'
-    })
-    password: string;
-    
+  @IsEmail()
+  @ApiProperty({
+    description: 'Email do Usuário',
+  })
+  email: string;
+
+  @ApiProperty({
+    description: 'Senha do Usuário',
+  })
+  password: string;
+
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,7 +2,10 @@ export class User {
     id?: string;
     name: string;
     nickname: string;
+    cpf: string;
     email: string;
     password: string;
+    role:string;
+    
 
 }
