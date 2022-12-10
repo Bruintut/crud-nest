8cb7c6e73c540cf20aad82dd13de3591654aa200
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-12-10 13:24:09 -0300

    conventional commit

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,13 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsNumber, IsString, IsUrl } from 'class-validator';
 
 export class CreateGameDto {
  
+  @IsString()
+  @ApiProperty({description: 'Nome do jogo!', example:'God of War'})
   title: string;
+
+  @IsString()
+  @ApiProperty({description: 'Imagem do jogo!', 
+  example:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR05zO3uU9DxnoqNR1VbcwKzSoq3iND1uKHaA&usqp=CAU'})
   coverImageUrl: string;
+
+  @IsString()
+  @ApiProperty({description: 'Descrição do jogo!', example:'Jogo com tema de ação e aventura!'})
   description: string;
+
+  @IsNumber()
+  @ApiProperty({description: 'Ano de lançamento do jogo!', example:'21/10/2015'})
   year: number;
+
+  @IsNumber({maxDecimalPlaces: 2})
+  @ApiProperty({description: 'Nota do jogo!', example:'10'})
   imdbScore: number;
+
+  @IsString()
+  @IsUrl()
+  @ApiProperty({description: 'URL do trailer do jogo no youtube!'})
   trailerYouTubeUrl: string;
+
+  @IsString()
+  @IsUrl()
+  @ApiProperty({description: 'URL da gameplay do jogo no youtube!'})
   gameplayYouTubeUrl: string;
 }
--- src/procfile/dto/create-procfile.dto.ts
+++ src/procfile/dto/create-procfile.dto.ts
@@ -1,10 +1,22 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
+import { IsString, IsUrl } from "class-validator";
 
 export class CreateProcfileDto {
     
+    @ApiProperty({ description:'Nome do Perfil' })
+    @IsString()
     name: string
+
+    @ApiProperty({ description:'Nickname do Perfil' })
+    @IsString()
     title: string
+
+    @ApiProperty({ description:'Imagem do Perfil' })
+    @IsString()
+    @IsUrl()
     imageUrl: string
+
+    @ApiProperty({ description:'Id do User gerenciador do Perfil' })
+    @IsString()
     userId: string
 }
