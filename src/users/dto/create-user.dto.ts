14cd82b9e55ae194da74ef8aedfd205a7381ec49
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-12-10 13:34:04 -0300

    feat: alter Dto User

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ description: 'Nome do Usuário' })
@@ -18,7 +18,12 @@ export class CreateUserDto {
 
   @ApiProperty({ description: 'Senha do Usuário' })
   @IsString()
+  @MinLength(8)
   password: string;
+
+  @ApiProperty({ description: 'Confirmação de senha do Usuário' })
+  @IsString()
+  confirmPassword: string;
 
   @ApiProperty()
   @IsBoolean()
