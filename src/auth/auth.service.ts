d2d85e97db4c93bb017d7dbe826f47485e0fcefb
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-12-10 22:41:00 -0300

    service auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
   async login(loginDto: LoginDto): Promise<LoginResponseDto> {
+
+    const {email, password} = loginDto;
+
+    const user = await this.prisma.user.findUnique({where: {}});
     return{
       token: 'Teste',
       user: undefined,
