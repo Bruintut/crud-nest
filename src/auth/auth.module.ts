5aba6c13acf3fc2f90a22877ba27c3d3ae49b6a8
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-12-10 23:50:21 -0300

    feat: implement strategy and guard in auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,16 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt/dist';
+import { PassportModule } from '@nestjs/passport/dist';
+import { JwtStrategy } from './jwt.strategy';
 
 
 @Module({
-  imports: [PrismaModule, JwtModule.register({
+  imports: [PrismaModule, PassportModule.register({defaultStrategy: 'jwt'}), JwtModule.register({
     secret: process.env.JWT_SECRET,
     signOptions: {expiresIn: '24h'}
   })],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
